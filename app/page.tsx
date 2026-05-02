4761400f5d109366781bbbb9ef27ee032161e88e
Roman <korvy85@gmail.com>
2026-05-02 19:24:21 +0300

    add globals styles and component button

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,7 @@ import styles from './App.module.css';
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <p>Hi123123123</p>
-      </main>
+      <main className={styles.main}></main>
     </div>
   );
 }
