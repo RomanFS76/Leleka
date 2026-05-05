e13a6c2d1190fa8a1cffe6cbd6ab48b7216d838f
Roman <korvy85@gmail.com>
2026-05-05 22:08:27 +0300

    feat(header): implement header with logo and burger menu

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import Link from 'next/link';
+import css from './Header.module.css';
+import Image from 'next/image';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className={css.header}>
+      <div className={css.flex}>
+        <Link className={css.link} href="/" aria-label="Leleka">
+          <Image src="/img/logo.svg" width={24} height={24} alt="logo" />
+          <Image src="/img/Лелека.svg" width={49} height={11} alt="logo" />
+        </Link>
+        <Image src="/img/burger.png" width={32} height={32} alt="menu" />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
